2ae8e2d6d4e7e58063dd0972a7a5f604d5638aa6
lorenadle <DOBRICALORENA12@stud.ase.ro>
2025-10-23 12:10:21 +0300

    adaugare getteri si setteri

--- E2.cpp
+++ E2.cpp
@@ -5,14 +5,14 @@ class Librarie {
 private:
 	char* numeLibrarie;
 	int nrCarti = 0;
-	char numeLibrar;
+	char* numeLibrar;
 
 public:
 
 	char* getNumeLibrarie() {
 
 		return this->numeLibrarie;
-	};
+	}
 
 	void setNumeLibrarie(const char* numeLibrarie) {
 		if (strlen(numeLibrarie)>0)
@@ -24,13 +24,43 @@ public:
 
 	}
 
+	char* getNumeLibrar() {
+		return this->numeLibrar;
+	}
+
+	void setNumeLibrar(const char* numeLibrar) {
+		if (numeLibrar != nullptr) {
+			this->numeLibrar = new char[strlen(numeLibrar) + 1];
+			strcpy_s(this->numeLibrar, strlen(numeLibrar) + 1, numeLibrar);
 
 
+		}
+	}
+
+	int getNrCarti() {
+
+		return this->nrCarti;
+
+	}
+
+
+	void setNrCarti(int nrCarti) {
+		if (nrCarti > 0) {
+			this->nrCarti = nrCarti;
+		}
+
+	}
 
 };
 void main() {
 	Librarie librarie;
 	librarie.setNumeLibrarie("Libraria Casa");
 	cout << librarie.getNumeLibrarie() << endl;
+
+	librarie.setNumeLibrar("Ioana popescu");
+	cout << "Libraria " << librarie.getNumeLibrarie() << " are ca librar astazi pe " << librarie.getNumeLibrar() << endl;
+
+	librarie.setNrCarti(10);
+	cout << "Libraria " << librarie.getNumeLibrarie() << " are ca librar astazi pe " << librarie.getNumeLibrar() << "si are in stoc " << librarie.getNrCarti() << " carti." << endl;
 
 }
