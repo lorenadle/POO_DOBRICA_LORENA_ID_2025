0c8d5bf26f6d094794ccaba066c1fcbbc6d33987
lorenadle <DOBRICALORENA12@stud.ase.ro>
2025-10-26 22:06:05 +0200

    corectie

--- E2.cpp
+++ E2.cpp
@@ -30,13 +30,16 @@ public:
 	}
 
 	void setNumeLibrar(const char* numeLibrar) {
-		if (numeLibrar != nullptr) {
+		if (strlen(numeLibrar) > 0) {
+			if (numeLibrar != nullptr) {
+
+
+				delete[]this->numeLibrar;
+			}
 			this->numeLibrar = new char[strlen(numeLibrar) + 1];
 			strcpy_s(this->numeLibrar, strlen(numeLibrar) + 1, numeLibrar);
-		
-
-
 		}
+					
 	}
 
 	int getNrCarti() {
@@ -57,31 +60,45 @@ public:
 
 
 	~Librarie() {
-		if (this->numeLibrarie != NULL) {
+		if (this->numeLibrarie != nullptr) {
 			delete[]this->numeLibrarie;
 		}
 }
 
-		Librarie() {
-			this->numeLibrarie = new char[strlen("Libraria Piticii") + 1];
-			strcpy_s(this->numeLibrarie, strlen("Libraria Piticii") + 1, numeLibrarie);
+	Librarie() {
+		if (strlen(numeLibrar) > 0) 
+		{
+			if (this->numeLibrarie != nullptr)
+		{
+			delete[]this->numeLibrarie;
+		}
+		this->numeLibrarie = new char[strlen("Libraria Piticii") + 1];
+		strcpy_s(this->numeLibrarie, strlen("Libraria Piticii") + 1, numeLibrarie);
+
+	
+			if (this->numeLibrar != nullptr)
+			{
+				delete[]this->numeLibrar;
+			}
 			this->numeLibrar = new char[strlen(numeLibrar) + 1];
 			strcpy_s(this->numeLibrar, strlen("Andreea Marin") + 1, numeLibrar);
 			this->nrCarti = 25;
 		}
+	}
 
 	
 };
 void main() {
 	Librarie librarie;
+	//Librarie  Librarie();
 	librarie.setNumeLibrarie("Libraria Casa");
 	cout << librarie.getNumeLibrarie() << endl;
 
-	librarie.setNumeLibrar("Ioana popescu");
-	cout << "Libraria " << librarie.getNumeLibrarie() << " are ca librar astazi pe " << librarie.getNumeLibrar() << endl;
+	//librarie.setNumeLibrar("Ioana popescu");
+	//cout << "Libraria " << librarie.getNumeLibrarie() << " are ca librar astazi pe " << librarie.getNumeLibrar() << endl;
 
 	librarie.setNrCarti(10);
 	cout << "Libraria " << librarie.getNumeLibrarie() << " are ca librar astazi pe " << librarie.getNumeLibrar() << " si are in stoc " << librarie.getNrCarti() << " carti." << endl;
 
-	Librarie  Librarie();
+	
 }
